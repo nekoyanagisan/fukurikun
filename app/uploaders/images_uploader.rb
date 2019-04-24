a930949a6de090ca442e9d9d27dec0b2d83529d5
koheiobata <kohei0216.2@gmail.com>
2019-04-24 12:42:05 +0900

    heroku error handling

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -4,12 +4,10 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
+  if Rails.env.production?
     storage :fog
+  else
+    storage :file
   end
 
   # Override the directory where uploaded files will be stored.
@@ -41,7 +39,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg png)
+    %w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
